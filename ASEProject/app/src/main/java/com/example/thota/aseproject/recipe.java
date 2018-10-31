70b2ec544b463ab4c5b997d13da4249c6d268d61
sumanthssv1996 <42655896+sumanthssv1996@users.noreply.github.com>
2018-10-31 03:34:09 -0500

    Update ASEProject/app/src/main/java/com/example/thota/aseproject/recipe.java

--- ASEProject/app/src/main/java/com/example/thota/aseproject/recipe.java
+++ ASEProject/app/src/main/java/com/example/thota/aseproject/recipe.java
@@ -1,5 +1,5 @@
 package com.example.thota.aseproject;
-
+// this page is used to display the recipies from the added items.
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
