7a9afaaf5f8be3e5076e2c1946482d1faf672ddd
Nikhilkrishnas <42650316+Nikhilkrishnas@users.noreply.github.com>
2018-11-05 13:20:34 -0600

    11

---
+++ CS5551_4MUSKETEERS_PROJECT-master/ASEProject/app/src/main/java/com/example/thota/aseproject/Home.java
@@ -0,0 +1,34 @@
+package com.example.thota.aseproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Home extends AppCompatActivity {
+    Button scan,additem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        scan=(Button)findViewById(R.id.ScanButton);
+        additem=(Button)findViewById(R.id.addItem);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent scanner= new Intent(Home.this,Scan.class);
+                startActivity(scanner);
+            }
+        });
+        //we are calling the addList activity
+        additem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addlistitem = new Intent(Home.this,addList.class);
+                addlistitem.putExtra("item","");
+                startActivity(addlistitem);
+            }
+        });
+    }
+}
