e39054e02e60dd1eec2dc4966da4ac0aae877ae7
Nikhilkrishnas <42650316+Nikhilkrishnas@users.noreply.github.com>
2018-12-02 12:47:33 -0600

    upload page 3

--- ASEProject increment 2/app/src/main/java/com/example/thota/aseproject/Home.java
+++ ASEProject increment 2/app/src/main/java/com/example/thota/aseproject/Home.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+// On adding the items into the list.
 public class Home extends AppCompatActivity {
     Button scan,additem;
     @Override
